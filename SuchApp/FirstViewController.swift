0a03e058406a11ccd0de05e823b7821ebcf3e8bf
Steffen Matthischke <steffen.matthischke@gmail.com>
2014-06-03 10:55:11 +0200

    Initial Commit

---
+++ SuchApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SuchApp
+//
+//  Created by Steffen on 03.06.14.
+//  Copyright (c) 2014 Steffen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
